bd1001afe2063f8a4c9a9f703df74d66ab696688
microdog <dlangu0393@gmail.com>
2016-06-14 12:28:10 +0800

    Cache Jinja2's Environment object in Jinja2Loader and accept initialization options

--- test/test.py
+++ test/test.py
@@ -8,7 +8,8 @@ import jinja2
 class LoaderTest(unittest.TestCase):
     def setUp(self):
         templates_path = 'test/templates/'
-        self.template_obj = Jinja2Loader(templates_path).load('page.html')
+        self.loader = Jinja2Loader(templates_path, jinja2_env_options={'newline_sequence': '\r\n'})
+        self.template_obj = self.loader.load('page.html')
 
     def test_load_template(self):
         self.assertIsInstance(self.template_obj, jinja2.Template)
@@ -16,6 +17,16 @@ class LoaderTest(unittest.TestCase):
     def test_generete_html(self):
         html_code = self.template_obj.generate(name='hi')
         self.assertIn('hi', html_code)
+
+    def test_get_jinja2_environment(self):
+        self.assertEqual(self.template_obj.environment, self.loader.get_jinja2_environment())
+
+    def test_cached_jinja2_environment(self):
+        template_obj2 = self.loader.load('page.html')
+        self.assertEqual(self.template_obj.environment, template_obj2.environment)
+
+    def test_jinja2_env_options(self):
+        self.assertEqual(self.loader.get_jinja2_environment().newline_sequence, '\r\n')
 
 
 if __name__ == '__main__':
--- tornado_jinja2/integration.py
+++ tornado_jinja2/integration.py
@@ -17,10 +17,24 @@ Environment.template_class = FixedTemplate
 
 class Jinja2Loader(tornado.template.Loader):
     """ inherit form tornado.template.Loader
-    Implementing customized Template Loader of for tornado to generate\
-            jinja2 template
-    """
-    def _create_template(self, name):
-        env = Environment(loader=FileSystemLoader(self.root))
-        return env.get_template(name)
+    Implementing customized Template Loader of for tornado to generate
+    Jinja2 template.
 
+    A dictionary may be passed using `jinja2_env_options` parameter, which
+    will be used as keyword arguments to create the Jinja2's Environment object.
+    Additional arguments are passed to tornado.template.Loader.
+    """
+
+    def __init__(self, root_directory, **kwargs):
+        env_options = kwargs.pop('jinja2_env_options') or {}
+
+        super(Jinja2Loader, self).__init__(root_directory, **kwargs)
+
+        env_options['loader'] = FileSystemLoader(self.root)
+        self._jinja2_env = Environment(**env_options)
+
+    def get_jinja2_environment(self):
+        return self._jinja2_env
+
+    def _create_template(self, name):
+        return self._jinja2_env.get_template(name)
